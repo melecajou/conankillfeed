564b516be2f1f7092a6692e0bfc6a6ae6a851d30
melecajou <eduardo.hoesker@gmail.com>
2025-08-15 14:56:12 -0300

    feat: Add .gitignore and remove sensitive files

--- config.py
+++
@@ -1,9 +0,0 @@
-# Para o Bot de Killfeed (killfeed_bot.py)
-KILLFEED_BOT_TOKEN = "MTM5NjIxMjc2MzI4MTQ1NzI5NQ.GdkeRA.JALMNyDsRu94G0niiWgLFrV_tv5kU-HJA5IW3Q"
-KILLFEED_CHANNEL_ID = 1396214175654481971
-SIPTAH_KILLFEED_CHANEL_ID = 1405243396569825400
-CONAN_SAVED_PATH = "/home/steam/conan_exiles/ConanSandbox/Saved/"
-SIPTAH_SAVED_PATH = "/home/steam/conan_exiles_siptah/ConanSandbox/Saved/"
-LAST_EVENT_TIME_FILE = "/home/steam/bots/Killfeed/last_event_time.txt"
-SIPTAH_LAST_EVENT_TIME_FILE = "/home/steam/bots/Killfeed/siptah_last_event_time.txt"
-SPAWNS_DB_PATH = "/home/steam/bots/Killfeed/spawns.db"
